4a3bd12288c169a56790dd41ee105dddc866e851
Sai Kiran <neelakantamsaikiran2@gmail.com>
2025-11-28 11:01:07 +0530

    fixed the type issue in test file

--- dist/bin.js
+++ dist/bin.js
@@ -1,4 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const _1 = require(".");
-_1.app.listen(4000);
+_1.app.get("/", (req, res) => {
+    res.send("Server is running on port 4000");
+});
+_1.app.listen(4000, () => {
+    console.log("🚀 Server is running on port 4000");
+});
--- src/bin.ts
+++ src/bin.ts
@@ -1,3 +1,10 @@
 import { app } from ".";
+import type { Request, Response } from "express";
 
-app.listen(4000)
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server is running on port 4000");
+});
+
+app.listen(4000, () => {
+  console.log("🚀 Server is running on port 4000");
+});
